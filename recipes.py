ab821c43994774fc8887486ef611dbd14ec343cf
kazminaaleksandra <kazminaaleksandra07@gmail.com>
2026-06-04 00:28:35 +0300

    Update recipes.py
    добавила класс Ingredient

--- recipes.py
+++ recipes.py
@@ -1,1 +1,31 @@
+class Ingredient:
+    def __init__(self, name, quantity, unit):
+        self.name = name
+        self.quantity = quantity
+        self.unit = unit
 
+    @property
+    def quantity(self):
+        return self._quantity
+
+    @quantity.setter
+    def quantity(self, value):
+        value = float(value)
+        if value > 0:
+            self._quantity =  value
+        else:
+            raise ValueError(
+                "Количество должно быть положительным"
+            )
+
+    def __str__(self):
+        return f"{self.name}: {self.quantity} {self.unit}"
+
+    def __repr__(self):
+        return f"Ingredient('{self.name}', {self.quantity}, '{self.unit}')"
+
+    def __eq__(self, other):
+        return self.name == other.name and self.unit == other.unit
+        
+        
+        
