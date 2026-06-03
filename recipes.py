58ba205e272b8f5895f7fcc2743b7bc89d816d5e
kazminaaleksandra <kazminaaleksandra07@gmail.com>
2026-06-04 00:32:05 +0300

    Update recipes.py
    добавила новый класс Recipe

--- recipes.py
+++ recipes.py
@@ -26,6 +26,45 @@ class Ingredient:
 
     def __eq__(self, other):
         return self.name == other.name and self.unit == other.unit
+
+class Recipe:
+    def __init__(self, title, ingredients):
+        self.title = title
+        self.ingredients = ingredients
+
+    def add_ingredient(self, ingredient: Ingredient):
+        for added in self.ingredients:
+            if added == ingredient:
+                added.quantity += ingredient.quantity
+                return
+        self.ingredients.append(ingredient)
+
+    @staticmethod
+    def is_valid_ratio(ratio):
+        try:
+            return float(ratio) > 0
+        except (ValueError, TypeError):
+            return False
+
+    def scale(self, ratio: float):
+        if not Recipe.is_valid_ratio(ratio):
+            raise ValueError(
+                f"Некорректное соотношение: {ratio}"
+            )
+        scaled_ingredients = [
+            Ingredient(ingredient.name, ingredient.quantity * ratio, ingredient.unit)
+            for ingredient in self.ingredients
+        ]
+    
+        return Recipe(self.title, scaled_ingredients)
+
+    def __len__(self):
+        return len(self.ingredients)
+
+    def __str__(self):
+        required_ingredients = "\n".join(f"{i+1}. {ingredient}" for i, ingredient in enumerate(self.ingredients))
+        return f"{self.title}, required ingredients: {required_ingredients}"
+    
         
         
         
